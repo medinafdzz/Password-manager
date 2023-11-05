aff4ffac851d605cdfb21830c930174e40f71e43
medinafdzz <suso121163@gmail.com>
2023-11-05 15:36:49 +0100

    Password-Manager

--- Manager.py
+++ Manager.py
@@ -142,3 +142,4 @@ y = (window.winfo_screenheight() // 2) - (window_height // 2)
 window.geometry('{}x{}+{}+{}'.format(window_width, window_height, x, y))
 
 window.mainloop()
+input("Presiona Enter para salir")
